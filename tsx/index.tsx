6f472fedba3a2054fee9ca2207458d6138f71634
Ahmad Kanawi <ahmad@mosey.systems>
2018-10-28 03:06:55 -0700

    adds some more cli support

--- tsx/index.tsx
+++ tsx/index.tsx
@@ -1,85 +1,94 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import '../sass/main.scss'
+import { CLIRequestBody, CLIResponse, CLIResponseType } from './models'
 import { TTY } from './tty'
-
-export enum CLIResponseType {
-    Error = 'ERROR',
-    Success = 'SUCCESS',
-    Info = 'INFO',
-}
-
-interface CLIResponse {
-    type: CLIResponseType
-    body: string[]
-}
 
 export type CLISubmitter = (input: string) => Promise<CLIResponse>
 
 class App extends React.Component<
-    {},
-    { ttyContent: string[], ttyType: CLIResponseType }
-    > {
-    public state = {
-        ttyContent: [
-            '> HeyoThere, barleysauce... \n',
-            '> Not much to see here yet \n',
-            '> But maybe, there will be stuff...at some point. \n',
-        ],
-        ttyType: CLIResponseType.Info,
+  {},
+  { ttyContent: string[]; ttyType: CLIResponseType }
+> {
+  public state = {
+    ttyContent: [
+      '> HeyoThere, barleysauce... \n',
+      '> Not much to see here yet \n',
+      '> But maybe, there will be stuff...at some point. \n'
+    ],
+    ttyType: CLIResponseType.Standard
+  }
+
+  public render() {
+    return (
+      <>
+        <header>
+          <h1>A website by Ahmad</h1>
+        </header>
+        <main>
+          <TTY type={this.state.ttyType} onSubmit={this.onSubmit}>
+            {this.state.ttyContent}
+          </TTY>
+        </main>
+      </>
+    )
+  }
+
+  private onSubmit = async (input: string): Promise<CLIResponse> => {
+    try {
+      const res = await fetch('https://mosey.systems/api/cli', {
+        body: JSON.stringify(buildBody(input)),
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json; charset=utf-8'
+        },
+        method: 'POST'
+      })
+
+      switch (res.status) {
+        case 200:
+          const { content, type } = await res.json()
+          // tslint:disable-next-line:no-console
+          console.log({ content, type })
+          return this.dispatchResponse(content, type)
+        case 412:
+          return this.dispatchResponse(
+            ['Unrecognized Server Response 🙁  '],
+            CLIResponseType.Error
+          )
+        default:
+          return this.dispatchResponse(
+            ['Unrecognized Server Response 🙁  '],
+            CLIResponseType.Error
+          )
+      }
+    } catch (err) {
+      // tslint:disable-next-line:no-console
+      console.warn(err)
+      return this.dispatchResponse(
+        ['Network Error. Could not reach remote system.  '],
+        CLIResponseType.Error
+      )
     }
+  }
 
-    public render() {
-        return (
-            <>
-                <header>
-                    <h1>A website by Ahmad</h1>
-                </header>
-                <main>
-                    <TTY type={this.state.ttyType} onSubmit={this.onSubmit}>
-                        {this.state.ttyContent}
-                    </TTY>
-                </main>
-            </>
-        )
-    }
-
-    private onSubmit = async (input: string): Promise<CLIResponse> => {
-        try {
-            const res = await fetch(
-                'mosey.sytems/api/cli',
-                {
-                    body: input,
-                    method: 'POST',
-                },
-            )
-
-            if (res.status === 200) {
-                const { body, type } = await res.json()
-                this.setState({ ttyContent: body, ttyType: type })
-                return Promise.resolve({ body, type })
-            }
-
-            return this.dispatchResponse(
-                ['Unrecognized Server Response 🙁 '],
-                CLIResponseType.Error,
-            )
-
-        } catch (err) {
-            return this.dispatchResponse(
-                ['Network Error. Could not reach remote system.'],
-                CLIResponseType.Error,
-            )
-        }
-    }
-
-    private dispatchResponse = (body: CLIResponse['body'], type: CLIResponseType) => {
-        this.setState({ ttyContent: body, ttyType: type })
-        return Promise.resolve({ body, type })
-    }
+  private dispatchResponse = (
+    content: CLIResponse['content'],
+    type: CLIResponseType
+  ) => {
+    this.setState({ ttyContent: content, ttyType: type })
+    return Promise.resolve({ content, type })
+  }
 }
 
-ReactDOM.render(
-    <App />,
-    document.getElementById('root') as HTMLElement,
-)
+ReactDOM.render(<App />, document.getElementById('root') as HTMLElement)
+
+const buildBody = (input: string): CLIRequestBody => ({
+  input,
+  language:
+    navigator &&
+    ((navigator.languages && navigator.languages.slice()[0]) ||
+      navigator.language),
+  referrer: document.referrer,
+  userAgent: navigator.userAgent
+})
--- tsx/tty.tsx
+++ tsx/tty.tsx
@@ -1,185 +1,199 @@
 import * as React from 'react'
-import { CLIResponseType, CLISubmitter } from '.'
+import { CLISubmitter } from '.'
+import { CLIResponseType } from './models'
 
 const DELAY = 100
-const SLOW_CHARS = new Set([
-    '.',
-    ',',
-    '\n',
-])
+const SLOW_CHARS = new Set(['.', ',', '\n'])
 
 export class TTY extends React.Component<
-    { children: string[], type: CLIResponseType, onSubmit: CLISubmitter },
-    { clicked: boolean, showInput: boolean }
-    > {
-    public state = { clicked: false, showInput: false }
+  { children: string[]; type: CLIResponseType; onSubmit: CLISubmitter },
+  { clicked: boolean; showInput: boolean }
+> {
+  public state = { clicked: false, showInput: false }
 
-    public render() {
-        return (
-            <div
-                className={`tty ${this.state.clicked ? 'tty--clicked' : ''}`}
-                onClick={this.showInput}
-                onMouseEnter={this.showInput}
-                onFocus={this.showInput}
-                tabIndex={1}
-            >
-                <TTYOutput type={this.props.type}>
-                    {this.props.children}
-                </TTYOutput>
-                {this.state.showInput && <TTYInput onSubmit={this.props.onSubmit} />}
-            </div>
-        )
-    }
+  public render() {
+    return (
+      <div
+        className={`tty ${this.state.clicked ? 'tty--clicked' : ''}`}
+        onClick={this.showInput}
+        onMouseEnter={this.showInput}
+        onFocus={this.showInput}
+        tabIndex={1}
+      >
+        <TTYOutput type={this.props.type}>{this.props.children}</TTYOutput>
+        {this.state.showInput && <TTYInput onSubmit={this.props.onSubmit} />}
+      </div>
+    )
+  }
 
-    private showInput = () => this.setState(
-        { clicked: true },
-        () => window.setTimeout(
-            () => this.setState({ showInput: true }),
-            300,
-        ),
+  private showInput = () =>
+    this.setState({ clicked: true }, () =>
+      window.setTimeout(() => this.setState({ showInput: true }), 300)
     )
 }
 
 class TTYInput extends React.Component<
-    { onSubmit: CLISubmitter },
-    { error: boolean, value: string }
-    > {
-    public state = { error: false, value: '' }
+  { onSubmit: CLISubmitter },
+  { type: CLIResponseType; value: string }
+> {
+  public state = { type: CLIResponseType.Standard, value: '' }
 
-    public render() {
-        return (
-            <form
-                className={`tty__input ${this.state.error ? 'tty__input--error' : ''}`}
-                onSubmit={this.onSubmit}
-            >
-                <label htmlFor="tty__input">&gt; </label>
-                <input
-                    id="tty__input"
-                    autoFocus
-                    autoComplete="off"
-                    spellCheck={false}
-                    type="text"
-                    value={this.state.value}
-                    onChange={(e) => this.setState({ error: false, value: e.target.value })}
-                />
-            </form>
-        )
+  public render() {
+    return (
+      <form
+        className={`tty__input ${
+          this.state.type === CLIResponseType.Error ? 'tty__input--error' : ''
+        } ${
+          this.state.type === CLIResponseType.Info ? 'tty__input--info' : ''
+        } ${
+          this.state.type === CLIResponseType.Success
+            ? 'tty__input--success'
+            : ''
+        }`}
+        onSubmit={this.onSubmit}
+      >
+        <label htmlFor="tty__input">&gt; </label>
+        <input
+          id="tty__input"
+          autoFocus
+          autoComplete="off"
+          spellCheck={false}
+          type="text"
+          value={this.state.value}
+          onChange={(e) => this.setState({ value: e.target.value })}
+        />
+      </form>
+    )
+  }
+
+  private onSubmit = async (e: React.SyntheticEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    const valueToSubmit = this.state.value
+    this.setState({ value: '' })
+
+    try {
+      const { type } = await this.props.onSubmit(valueToSubmit)
+      this.setState({ type })
+    } catch (err) {
+      this.setState({ type: CLIResponseType.Error })
     }
-
-    private onSubmit = async (e: React.SyntheticEvent<HTMLFormElement>) => {
-        e.preventDefault()
-
-        const valueToSubmit = this.state.value
-        this.setState({ value: '' })
-
-        try {
-            const { type } = await this.props.onSubmit(valueToSubmit)
-            this.setState({ error: type === CLIResponseType.Error })
-        } catch (err) {
-            this.setState({ error: true })
-        }
-    }
+  }
 }
 
 interface TTYOutputProps {
-    children: string[],
-    type: CLIResponseType,
-    onAllLinesComplete?: () => void,
+  children: string[]
+  type: CLIResponseType
+  onAllLinesComplete?: () => void
 }
 
 export class TTYOutput extends React.Component<
-    TTYOutputProps,
-    { currentLine: number }
-    > {
-    public state = { currentLine: 1 }
+  TTYOutputProps,
+  { currentLine: number }
+> {
+  public state = { currentLine: 1 }
 
-    public componentDidUpdate(prevProps: TTYOutputProps) {
-        if (prevProps.children !== this.props.children) {
-            this.setState({ currentLine: 1 })
-        }
+  public componentDidUpdate(prevProps: TTYOutputProps) {
+    if (prevProps.children !== this.props.children) {
+      this.setState({ currentLine: 1 })
     }
+  }
 
-    public render() {
-        return (
-            <div className={`tty__output ${this.props.type === CLIResponseType.Error ? 'tty__output--error' : ''}`}  >
-                {(this.props.children as string[]).slice(0, this.state.currentLine).map((line, idx) => (
-                    <TTYLine key={idx} onLineComplete={this.startNextLine}>
-                        {line}
-                    </TTYLine>
-                ))}
-            </div>
-        )
-    }
+  public render() {
+    return (
+      <div
+        className={`tty__output ${
+          this.props.type === CLIResponseType.Error ? 'tty__output--error' : ''
+        } ${
+          this.props.type === CLIResponseType.Info ? 'tty__output--info' : ''
+        } ${
+          this.props.type === CLIResponseType.Success
+            ? 'tty__output--success'
+            : ''
+        }`}
+      >
+        {(this.props.children as string[])
+          .slice(0, this.state.currentLine)
+          .map((line, idx) => (
+            <TTYLine key={idx} onLineComplete={this.startNextLine}>
+              {line}
+            </TTYLine>
+          ))}
+      </div>
+    )
+  }
 
-    private startNextLine = () => this.setState(
-        (prevState) => ({ currentLine: prevState.currentLine + 1 }),
-        () => this.state.currentLine > this.props.children.length && this.props.onAllLinesComplete
-            ? this.props.onAllLinesComplete()
-            : undefined,
+  private startNextLine = () =>
+    this.setState(
+      (prevState) => ({ currentLine: prevState.currentLine + 1 }),
+      () =>
+        this.state.currentLine > this.props.children.length &&
+        this.props.onAllLinesComplete
+          ? this.props.onAllLinesComplete()
+          : undefined
     )
 }
 
-interface TTYLineProps { children: string, onLineComplete: () => void }
-
-class TTYLine extends React.Component<
-    TTYLineProps,
-    { currentIndex: number }
-    > {
-    public state = { currentIndex: 0 }
-    private handle: number
-
-    public componentDidMount() {
-        this.setTick(DELAY)
-    }
-
-    public componentWillUnmount() {
-        clearTimeout(this.handle)
-    }
-
-    public componentDidUpdate(prevProps: TTYLineProps) {
-        if (prevProps.children !== this.props.children) {
-            this.setState({ currentIndex: 0 }, () => this.setTick(DELAY))
-        }
-    }
-
-    public render() {
-        return (
-            <TTYPresentation>
-                {this.props.children.slice(0, this.state.currentIndex)}
-            </TTYPresentation>
-        )
-    }
-
-    private setTick = (delay: number) => {
-        this.handle = window.setTimeout(
-            () => this.tick(),
-            delay,
-        )
-    }
-
-    private tick = () => {
-        const { children, onLineComplete } = this.props
-        const { currentIndex } = this.state
-        let delay = DELAY
-
-        if (SLOW_CHARS.has(children[currentIndex])) {
-            delay = delay * 3
-        }
-
-        if (this.state.currentIndex + 1 < this.props.children.length) {
-            this.setTick(delay)
-        } else {
-            onLineComplete()
-        }
-
-        this.setState((prevState) => ({ currentIndex: prevState.currentIndex + 1 }))
-    }
+interface TTYLineProps {
+  children: string
+  onLineComplete: () => void
 }
 
-const TTYPresentation = ({ children }: { children: string | React.ReactNode[] }) => (
-    <div className="tty__line"
-        data-last={children.slice(-1)}
-    >
-        {children.slice(0, -2)}
-    </div>
+class TTYLine extends React.Component<TTYLineProps, { currentIndex: number }> {
+  public state = { currentIndex: 0 }
+  private handle: number
+
+  public componentDidMount() {
+    this.setTick(DELAY)
+  }
+
+  public componentWillUnmount() {
+    clearTimeout(this.handle)
+  }
+
+  public componentDidUpdate(prevProps: TTYLineProps) {
+    if (prevProps.children !== this.props.children) {
+      this.setState({ currentIndex: 0 }, () => this.setTick(DELAY))
+    }
+  }
+
+  public render() {
+    return (
+      <TTYPresentation>
+        {this.props.children.slice(0, this.state.currentIndex)}
+      </TTYPresentation>
+    )
+  }
+
+  private setTick = (delay: number) => {
+    this.handle = window.setTimeout(() => this.tick(), delay)
+  }
+
+  private tick = () => {
+    const { children, onLineComplete } = this.props
+    const { currentIndex } = this.state
+    let delay = DELAY
+
+    if (SLOW_CHARS.has(children[currentIndex])) {
+      delay = delay * 3
+    }
+
+    if (this.state.currentIndex + 1 < this.props.children.length) {
+      this.setTick(delay)
+    } else {
+      onLineComplete()
+    }
+
+    this.setState((prevState) => ({ currentIndex: prevState.currentIndex + 1 }))
+  }
+}
+
+const TTYPresentation = ({
+  children
+}: {
+  children: string | React.ReactNode[]
+}) => (
+  <div className="tty__line" data-last={children.slice(-1)}>
+    {children.slice(0, -2)}
+  </div>
 )
