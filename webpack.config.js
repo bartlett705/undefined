d7ec939178c0e64eacd4cc141d5ed8e0a87d3a7c
Ahmad Kanawi <ahmad@mosey.systems>
2019-02-03 22:23:46 -0800

    adds copy-webpack-plugin (again?)

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,14 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const devMode = process.env.NODE_ENV !== 'production'
 const CleanWebPackPlugin = require('clean-webpack-plugin')
-const webpack = require('webpack')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 const plugins = [
   new CleanWebPackPlugin(['build'], {
     root: path.resolve(__dirname),
     verbose: true
   }),
+  new CopyWebpackPlugin([{ from: 'public/', to: '.' }]),
   new MiniCssExtractPlugin({
     filename: devMode ? '[name].css' : '[name].[hash].css',
     chunkFilename: devMode ? '[id].css' : '[id].[hash].css'
