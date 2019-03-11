201080d57245d36e2dc7959e0f91dc8964a542d1
Ahmad Kanawi <ahmad@mosey.systems>
2019-03-10 21:55:16 -0700

    add new secrets.tar

--- cypress/run.sh
+++ cypress/run.sh
@@ -1,3 +1,7 @@
+if [ -n "$CONFIG_KEY" ]; then 
+  openssl enc -aes-256-cbc -d -in secrets.tar.enc -out secrets.tar -k $CONFIG_KEY
+  tar -xvf secrets.tar
+fi
 NODE_ENV=staging npm start -- --silent &
 npx cypress run
 # after all tests finish running we need to kill npm start
