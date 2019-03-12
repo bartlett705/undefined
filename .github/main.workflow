5a3deaf75a3ea51dcdcc4ca8fcb156c1d59b5b8a
Ahmad Kanawi <ahmad@mosey.systems>
2019-03-11 21:16:25 -0700

    working on flows

--- .github/main.workflow
+++ .github/main.workflow
@@ -31,12 +31,9 @@ action "Unit Tests" {
 # }
 
 action "Deploy" {
-  uses = "actions/npm@59b64a598378f31e49cb76f27d6f3312b582f680"
+  uses = "bartlett705/npm-cy@817d541139ca06add505f5a48364f806b3c9ef44"
   needs = ["Unit Tests"]
   args = "run deploy:ci"
-  secrets = [
-    "TRAVIS_RSA",
-  ]
 }
 
 action "Notify End" {
--- scripts/cy-run.sh
+++ scripts/cy-run.sh
@@ -1,8 +1,8 @@
-if [ -n "$CONFIG_KEY" ]; then 
-  openssl --list-ciphers
-  openssl enc -aes-256-cbc -d -in secrets.tar.enc -out secrets.tar -k $CONFIG_KEY
-  tar -xvf secrets.tar
-fi
+# if [ -n "$CONFIG_KEY" ]; then 
+#   openssl --list-ciphers
+#   openssl enc -aes-256-cbc -d -in secrets.tar.enc -out secrets.tar -k $CONFIG_KEY
+#   tar -xvf secrets.tar
+# fi
 NODE_ENV=staging npm start -- --silent &
 npx cypress run
 # after all tests finish running we need to kill npm start
