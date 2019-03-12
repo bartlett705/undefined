41a27298e069935744f6afeb2d19684384938917
Ahmad Kanawi <ahmad@mosey.systems>
2019-03-11 22:25:25 -0700

    on moar time with the config

--- scripts/deploy-ci.sh
+++ scripts/deploy-ci.sh
@@ -6,8 +6,7 @@ ls -al ci
 echo "Decrpyt Done!"
 
 echo "Building Dist Pkg..."
-npm run build && echo "Copying Dist to remote host..." && scp -vvv -P 5235 -i travis_id_rsa -o UserKnownHostsFile=ci/known_hosts build/*.* travis@mosey.systems:/var/www/undefined/build
+npm run build && echo "Copying Dist to remote host..." && scp -v -P 5235 -i ci/travis_id_rsa -o UserKnownHostsFile=ci/known_hosts build/*.* travis@mosey.systems:/var/www/undefined/build
 echo "Cleaning up..."
-rm ci/travis_id_rsa
 rm -rf build
 echo Done!
