33c39458d3d7af1a9ff9f01b924b62739c30923f
Ahmad Kanawi <ahmad@mosey.systems>
2019-03-18 21:19:12 -0700

    copy hidden dir in public as well

--- scripts/deploy-ci.sh
+++ scripts/deploy-ci.sh
@@ -6,7 +6,7 @@ ls -al ci
 echo "Decrpyt Done!"
 
 echo "Building Dist Pkg..."
-npm run build && echo "Copying Dist to remote host..." && scp -v -P 5235 -i ci/travis_id_rsa -o UserKnownHostsFile=ci/known_hosts build/*.* travis@mosey.systems:/var/www/undefined/build
+npm run build && echo "Copying Dist to remote host..." && scp -v -P 5235 -i ci/travis_id_rsa -o UserKnownHostsFile=ci/known_hosts build/. travis@mosey.systems:/var/www/undefined/build
 echo "Cleaning up..."
 rm -rf build
 echo Done!
